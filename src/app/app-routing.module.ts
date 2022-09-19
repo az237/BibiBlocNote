bbc2fe210c142ceac89f3b0d5a2570cb28bb8ad5
chacha98 <kapawouchanelle@gmail.com>
2022-09-19 08:43:58 +0100

    accueil en cours

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from "./connexion/connexion.component";
 import { InscriptionComponent } from "./inscription/inscription.component";
 import { AccueilComponent } from "./accueil/accueil.component";
-
+import {NewnoteComponent} from "./newnote/newnote.component";
 const routes: Routes = [
   {path : '', component: ConnexionComponent},
-  {path : 'inscription', component: InscriptionComponent },
-  {path: 'accueil', component: AccueilComponent}
+  {path : 'inscription', component: InscriptionComponent},
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'new_note', component:NewnoteComponent}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import {FontAwesomeModule,FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { NewnoteComponent } from './newnote/newnote.component';
-
+import { fas } from '@fortawesome/free-solid-svg-icons';
+//import { far } from '@fortawesome/free-regular-svg-icons';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,10 +22,17 @@ import { NewnoteComponent } from './newnote/newnote.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgxBootstrapIconsModule.pick(allIcons),
+    FontAwesomeModule
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  constructor(private library: FaIconLibrary){
+    library.addIconPacks(fas);
+  }
+}
